d86bdb0f02e9865c9694385d1551fafcea300bb6
mycomputerworld99 <93781947+mycomputerworld99@users.noreply.github.com>
2022-06-16 06:05:39 +0500

    Update and rename setting.js to setting1.1.js

---
+++ setting1.1.js
@@ -0,0 +1,33 @@
+//<![CDATA[
+
+
+
+(function(d, t, e, m){
+    
+    // Async Rating-Widget initialization.
+    window.RW_Async_Init = function(){
+                
+        RW.init({
+            huid: "484262",
+            uid: "7e794e29e2f7d8a0e6db4315d73a9750",
+            options: { "style": "oxygen" } 
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+        l = d.location, ck = "Y" + t.getFullYear() + 
+        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+    if (d.getElementById(id)) return;              
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), "script", "rating-widget.com/"));
+
+
+
+
+//]]>
